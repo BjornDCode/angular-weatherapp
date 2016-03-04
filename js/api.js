57e07c24427366eac9a9f87a41974ac62c4606be
Bjorn <lindholmh@mail.com>
2016-03-04 18:50:34 +0000

    City ID's seperated to array

--- js/api.js
+++ js/api.js
@@ -1,7 +1,10 @@
 angular.module('firstApp.api', [])
   .factory('api', ['$http', function($http){
 
+    // Cities
+    var args = ['524901', '703448', '2643743', '2988507', '2950159', '3117735', '2618425', '2267057'];
+    
     // Create API Call with promise
-    return $http.get('http://api.openweathermap.org/data/2.5/group?id=524901,703448,2643743,2988507,2950159,3117735,2618425,2267057&units=metric&appid=44db6a862fba0b067b1930da0d769e98', { cache: true });
+    return $http.get('http://api.openweathermap.org/data/2.5/group?id=' + args.toString() + '&units=metric&appid=44db6a862fba0b067b1930da0d769e98', { cache: true });
 
   }]);
